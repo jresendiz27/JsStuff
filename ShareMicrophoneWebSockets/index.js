6960558195d9d0b8a2a056f9851fb0d97cd78e3b
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2015-09-14 20:40:44 -0500

    First attempt, using node as socket server

---
+++ ShareMicrophoneWebSockets/index.js
@@ -0,0 +1,56 @@
+var http = require("http"),
+  url = require("url"),
+  iolib = require("socket.io"),
+  fs = require("fs"),
+  error404 = function error404(response) {
+    response.writeHead(400, {'Content-Type': 'text/html'});
+    response.write("Something went wrong, check log for further information");
+    response.end();
+  },
+  server = http.createServer(function createServer(request, response) {
+    var path = url.parse(request.url).pathname;
+    switch (path) {
+      case "/":
+        console.log("Hello world");
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write("Hello World from Node server.");
+        response.end();
+        break;
+      case "/broadcast.html":
+        fs.readFile(__dirname + path, function readFile(err, data) {
+          if(err) {
+            console.error(err);
+            error404(response);
+          } else {
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(data);
+            response.end();
+          }
+        });
+        break;
+      case "/receive.html":
+        fs.readFile(__dirname + path, function readFile(err, data) {
+          if(err) {
+            console.error(err);
+            error404(response);
+          } else {
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(data);
+            response.end();
+          }
+        });
+        break;
+      default:
+        error404(response);
+    }
+  });
+server.listen(8888, function serverStarted() {
+  console.log("Server started at 8888");
+});
+var io = iolib.listen(server);
+io.sockets.on('connection', function connected(socket) {
+  socket.on('microphoneData', function microphoneData(data) {
+    console.log(data.length);
+    socket.emit('microphoneStreaming', data);
+  });
+});
